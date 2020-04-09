8b632ea8b7578916023ccbf6f23f77eb3834e404
MarkusJx <28785953+MarkusJx@users.noreply.github.com>
2020-04-09 10:46:30 +0200

    unix fix
    Signed-off-by: MarkusJx <28785953+MarkusJx@users.noreply.github.com>

--- src/CppJsLib.hpp
+++ src/CppJsLib.hpp
@@ -11,7 +11,7 @@
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
 #   define CPPJSLIB_WINDOWS
 #   undef CPPJSLIB_UNIX
-#elif defined(__LINUX__) || defined(__APPLE__) || defined (__CYGWIN__) || defined(__linux__)
+#elif defined(__LINUX__) || defined(__APPLE__) || defined (__CYGWIN__) || defined(__linux__) || defined(__FreeBSD__) || defined(unix) || defined(__unix) || defined(__unix__)
 #   define CPPJSLIB_UNIX
 #   undef CPPJSLIB_WINDOWS
 #endif
